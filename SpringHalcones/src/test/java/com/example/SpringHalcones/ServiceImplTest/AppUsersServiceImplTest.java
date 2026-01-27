c500bd1dcaa9e88be9f14818c89bf839c4c3a9aa
julixxx23 <jorellanao12@miumg.edu.gt>
2026-01-26 23:24:33 -0600

    Unit testing for AppUsersServiceImpl

--- SpringHalcones/src/test/java/com/example/SpringHalcones/ServiceImplTest/AppUsersServiceImplTest.java
+++ SpringHalcones/src/test/java/com/example/SpringHalcones/ServiceImplTest/AppUsersServiceImplTest.java
@@ -1,9 +1,14 @@
 package com.example.SpringHalcones.ServiceImplTest;
 
+import com.example.SpringHalcones.repository.AppUsersRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class AppUsersServiceImplTest {
+    @Mock
+    private AppUsersRepository appUsersRepository;
+
 
 }
