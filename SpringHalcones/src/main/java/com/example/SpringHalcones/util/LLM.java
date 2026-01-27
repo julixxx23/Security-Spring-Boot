02825192bb57b3ddd14084296e0ba69b3f1ea3b6
julixxx23 <jorellanao12@miumg.edu.gt>
2026-01-26 23:21:24 -0600

    Development of unit tests for the AppUsers ServiceImpl

--- SpringHalcones/src/main/java/com/example/SpringHalcones/util/LLM.java
+++
@@ -1,6 +0,0 @@
-package com.example.SpringHalcones.util;
-
-public class LLM {
-    System.Logger();
-
-}
---
+++ SpringHalcones/src/test/java/com/example/SpringHalcones/ServiceImplTest/AppUsersServiceImplTest.java
@@ -0,0 +1,9 @@
+package com.example.SpringHalcones.ServiceImplTest;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AppUsersServiceImplTest {
+
+}
